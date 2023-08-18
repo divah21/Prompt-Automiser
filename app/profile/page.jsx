38a808d4e0be3d6fbdff56ee81bffa34e01c4db7
divah21 <divahsmart@gmail.com>
2023-08-18 11:58:22 +0200

    update

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,9 +6,11 @@ import { useRouter } from "next/navigation";
 import Profile from "@components/Profile";
 
 const MyProfile = () => {
+  const router = useRouter();
    const {data: session} = useSession();
 
    const [posts, setPosts] = useState([]);
+
 
     useEffect(()=>{
         const fetchPosts = async()=>{
@@ -22,11 +24,22 @@ const MyProfile = () => {
 
       },[]);
 
-    const handleEdit =()=>{
-
+    const handleEdit =(post)=>{
+      router.push(`/update-prompt?id=${post.id}`)
     }
-    const handleDelete =() =>{
-            
+    const handleDelete = async(post) =>{
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
+        if(hasConfirmed){
+          try {
+            await fetch(`/api/prompt/${post._id.toString()}`,{
+              method: 'DELETE'
+            });
+
+            const filteredPosts = posts.filter((p)=>p._id !== post._id);
+          } catch (error) {
+            console.log(error)
+          }
+        }
     }  
   return (
     <Profile
